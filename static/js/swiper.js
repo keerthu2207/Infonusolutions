a15c5cc7819de1496a059c7f5bcda8c53628a017
keerthu2207 <keerthanaachandru2005@gmail.com>
2025-10-28 22:33:13 +0530

    mediaqueries updated

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -24,3 +24,20 @@ new Swiper('.card-wrapper', {
     }
   }
 });
+// new Swiper('.swiper', {
+//   loop: true,
+//   spaceBetween: 20, // adjust as needed
+//   slidesPerView: 3,
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+//     0: { slidesPerView: 1 },
+//     768: { slidesPerView: 2 },
+//     1024: { slidesPerView: 3 }
+//   }
+// });
